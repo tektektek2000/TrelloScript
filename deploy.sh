7e56bcde9fdaa289a49d6e9e6a2c9ed181177517
Norbert László <laszlo.norbi2000@hotmail.com>
2022-06-15 09:10:19 +0200

    Deploy script fixed

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ sudo git pull
 echo "--------------Building--------------"
 sudo dotnet publish --configuration Release
 echo "--------------Copying files--------------"
-sudo 'cp' -rf /home/tektek2000/TrelloScript/TrelloScriptServer/bin/Release/net6.0/publish/* /var/www/TrelloScriptServer/echo "--------------Starting service--------------"
+sudo 'cp' -rf /home/tektek2000/TrelloScript/TrelloScriptServer/bin/Release/net6.0/publish/* /var/www/TrelloScriptServer/
+echo "--------------Starting service--------------"
 sudo systemctl start kestrel-TrelloScriptServer.service
 echo "All done"
