c46fb8175627d9340c0d30fb8d18d34f41886c94
Norbert László <laszlo.norbi2000@hotmail.com>
2022-06-15 09:03:46 +0200

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "Deploy script v1.0"
+echo "--------------Stopping Service--------------"
+sudo systemctl stop kestrel-TrelloScriptServer.service
+echo "--------------Pulling--------------"
+sudo git pull
+echo "--------------Building--------------"
+sudo dotnet publish --configuration Release
+echo "--------------Copying files--------------"
+sudo 'cp' -rf /home/tektek2000/TrelloScript/TrelloScriptServer/bin/Release/net6.0/publish/* /var/www/TrelloScriptServer/echo "--------------Starting service--------------"
+sudo systemctl start kestrel-TrelloScriptServer.service
+echo "All done"
